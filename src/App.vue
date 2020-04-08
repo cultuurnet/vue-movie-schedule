2af1fc55548f77378deba3adf01fe0c41d6986ef
brampauwelyn <brampauw@gmail.com>
2020-04-08 09:37:27 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="publiq logo" class="logo" src="./assets/logo.png">
+    <h1>Movie Schedule</h1>
+    <p>Add your component here</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,6 +22,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+.logo{
+  width: 100px;
 }
 </style>
